1de80e3e05843f93583982ba6768ff6cace8ae58
Tomasz Ogrodowski <tom.ogrodowski@gmail.com>
2025-10-03 09:49:53 +0200

    Initial Commit

---
+++ AutoTranslate/AutoTranslateApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoTranslateApp.swift
+//  AutoTranslate
+//
+//  Created by itcraft on 03/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct AutoTranslateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
