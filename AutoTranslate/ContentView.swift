1020b9b16c3dd6c7631d6173a67a50b5981fc1e4
Tomasz Ogrodowski <tom.ogrodowski@gmail.com>
2025-10-03 10:02:05 +0200

    basic translation

--- AutoTranslate/ContentView.swift
+++ AutoTranslate/ContentView.swift
@@ -1,21 +1,32 @@
-//
-//  ContentView.swift
-//  AutoTranslate
-//
-//  Created by itcraft on 03/10/2025.
-//
-
 import SwiftUI
+import Translation
 
 struct ContentView: View {
+    
+    @State private var input = "Hello, world!"
+    
+    @State private var configuration =  TranslationSession.Configuration(
+        source: Locale.Language(
+            identifier: "en"
+        ),
+        target: Locale.Language(
+            identifier: "de"
+        )
+    )
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TextEditor(text: $input)
+            .font(.largeTitle)
+            .translationTask(configuration, action: translate)
+    }
+    
+    func translate(using session: TranslationSession) async {
+        do {
+            let result = try await session.translate(input)
+            print(result.targetText)
+        } catch {
+            print(error.localizedDescription)
         }
-        .padding()
     }
 }
 
